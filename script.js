39d9b1361ba0a94e54ca60e33ff927e3c467ff88
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2024-08-12 22:31:36 +0530

    good

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function showAbout(type){
     const homepage = document.getElementById('home')
     const aboutpage = document.getElementById('about')
+    const skillpage = document.getElementById('skill')
 
     if (type == 'about'){
         aboutpage.style.display = "flex";
         homepage.style.display = "none";
+        skillpage.style.display = "none";
     } else if(type == 'home'){
         aboutpage.style.display = "none";
         homepage.style.display = "flex";
+        skillpage.style.display = "none";
+    }else if(type == 'skill'){
+        aboutpage.style.display = "none";
+        homepage.style.display = "none";
+        skillpage.style.display = "block";
     }
 }
