864878e28bad912d1197a96cde783fffc2877af6
Jagadesh08Madhu <jagadesh2001kavi@gmail.com>
2024-08-19 13:50:07 +0530

    changese

--- script.js
+++ script.js
@@ -2,18 +2,40 @@ function showAbout(type){
     const homepage = document.getElementById('home')
     const aboutpage = document.getElementById('about')
     const skillpage = document.getElementById('skill')
+    const projectpage = document.getElementById('project')
+    const contactpage = document.getElementById('contact')
 
     if (type == 'about'){
         aboutpage.style.display = "flex";
         homepage.style.display = "none";
         skillpage.style.display = "none";
+        projectpage.style.display= "none";
+        contactpage.style.display= "none"
     } else if(type == 'home'){
         aboutpage.style.display = "none";
         homepage.style.display = "flex";
         skillpage.style.display = "none";
+        projectpage.style.display= "none";
+        contactpage.style.display= "none"
     }else if(type == 'skill'){
         aboutpage.style.display = "none";
         homepage.style.display = "none";
         skillpage.style.display = "block";
+        projectpage.style.display= "none";
+        contactpage.style.display= "none"
+    }else if (type == 'project'){
+        aboutpage.style.display = "none";
+        homepage.style.display = "none";
+        skillpage.style.display = "none";
+        projectpage.style.display= "block";
+        contactpage.style.display= "none"
+    }else if(type == 'contact'){
+        aboutpage.style.display = "none";
+        homepage.style.display = "none";
+        skillpage.style.display = "none";
+        projectpage.style.display= "none";
+        contactpage.style.display= "flex"
     }
 }
+
+
